2cddcb20e9c35351d71f47a5c9dd315da0b8d53c
kyorere374-del <kyorere374@gmail.com>
2025-10-20 15:19:01 +0700

    Tugas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Login } from './login/login';
+import { NgModule } from '@angular/core';
+import { Admin } from './admin/admin';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", redirectTo: "login", pathMatch: "full"},
+    { path:"dashboard", component: Dashboard},
+    { path: "login", component: Login},
+    { path: "admin", component: Admin},
+];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule]
+})
+export class AppRoutes {}
