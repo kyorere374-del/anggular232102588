c47e932c7d2c36f760bc33bf8f136a5f4f9cfeb3
kyorere374-del <kyorere374@gmail.com>
2025-10-27 14:17:33 +0700

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Dashboard } from './dashboard/dashboard';
 import { Login } from './login/login';
 import { NgModule } from '@angular/core';
 import { Admin } from './admin/admin';
+import { Dashboard2 } from './dashboard2/dashboard2';
 
 export const routes: Routes = [
     { path: "", redirectTo: "login", pathMatch: "full"},
     { path:"dashboard", component: Dashboard},
+    { path: "dashboard2", component: Dashboard2},
     { path: "login", component: Login},
     { path: "admin", component: Admin},
 ];
--- src/app/dashboard2/dashboard2.ts
+++ src/app/dashboard2/dashboard2.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { Sidebar } from "../sidebar/sidebar";
+import { Header } from "../header/header";
+import { Footer } from "../footer/footer";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard2',
-  imports: [],
+  imports: [Sidebar, Header, Footer,RouterModule],
   templateUrl: './dashboard2.html',
   styleUrl: './dashboard2.css'
 })
